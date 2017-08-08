06cfb196783fc7fbdbf480c8404b4d10e9bb37b3
pz <284270220@qq.com>
2017-08-08 19:54:20 +0800

    添加后缀排序：引言，相关工作，相关概念

--- chapters/auto/ch_3.el
+++ chapters/auto/ch_3.el
@@ -1,69 +1,51 @@
 (TeX-add-style-hook
  "ch_3"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("frontiersSCNS" "utf8")))
-   (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("setspace" "onehalfspacing")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
-    "frontiersSCNS"
-    "frontiersSCNS10"
-    "url"
-    "hyperref"
-    "lineno"
-    "microtype"
-    "subcaption"
-    "setspace"
-    "graphicx"
-    "algorithm"
-    "algorithmic"
-    "multirow"
-    "amsmath"
-    "color")
-   (TeX-add-symbols
-    "keyFont"
-    "firstAuthorLast"
-    "Authors"
-    "Address"
-    "corrAuthor"
-    "corrEmail")
+    "ws-ijprai"
+    "ws-ijprai10"
+    "enumerate")
    (LaTeX-add-labels
     "sec:introduction"
-    "sec:MM"
-    "fig:DM"
-    "sec:Dominant Point"
-    "fig:DAG"
-    "sec:Algorithm"
-    "sec:data structures"
-    "sec:successor table"
-    "fig:ST"
-    "sec:Node"
-    "sec:auxiliary"
-    "sec:leveled DAG"
-    "fig:Leveled-DAG"
-    "sec:PMA"
-    "alg:PMA"
-    "sec:complexity"
-    "sec:experiments"
-    "Test problems"
-    "sec:number"
-    "sec:times2"
-    "sec:conculsion"
-    "tab:times1"
-    "tab:memory1"
-    "tab:times2"
-    "tab:memory2")
-   (LaTeX-add-environments
-    '("varalgorithm" 1))
-   (LaTeX-add-bibliographies
-    "frontiers"))
+    "sec:stucture"
+    "sec:qsufsort"
+    "sec:dsufsort"
+    "fig:1"
+    "eq:ca1"
+    "eq:ca2"
+    "tab:data"
+    "tab:time")
+   (LaTeX-add-bibitems
+    "Manber"
+    "Karp"
+    "survey1"
+    "survey2"
+    "Copy_Cache"
+    "deep_shallow"
+    "qsufsort"
+    "bpr"
+    "RadixSA"
+    "KA"
+    "KS"
+    "KSP"
+    "Farach"
+    "two_stages"
+    "SA_IS_DS"
+    "SACA_K"
+    "em_1"
+    "em_2"
+    "em_3"
+    "t_quciksort"
+    "quicksort"
+    "DC32"
+    "dataset"))
  :latex)
 
